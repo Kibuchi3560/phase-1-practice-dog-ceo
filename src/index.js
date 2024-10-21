4e59e30875350c0ce649acf1788535d89efa1f5b
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-10-21 17:31:41 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4'; //Gets 4 random dog images
+let imagesContainer = document.getElementById('dog-image-container');
+fetch(imgUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    imagesArray = data.message;
+    imagesArray.forEach((image) => {
+      let imageElement = document.createElement('img');
+      imagesContainer.appendChild(imageElement);
+      imageElement.setAttribute('src', image);
+    });
+  });
+
+//Challenge 2
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+let breedsContainer = document.getElementById('dog-breeds');
+fetch(breedUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    breeds = data.message;
+    for (let breed in breeds) {
+      let breedName = document.createElement('li');
+      breedName.innerText = breed;
+      breedsContainer.appendChild(breedName);
+      //Challenge 3
+      breedName.addEventListener('click', () => {
+        breedName.style.color = 'red';
+      });
+    }
+  });
+//Challenge 4
+let firstLetterDropDown = document.querySelector('#breed-dropdown');
+firstLetterDropDown.addEventListener('change', () => {
+  const selectedLetter = firstLetterDropDown.value;
+  Array.from(breedsContainer.getElementsByTagName('li')).forEach((breed) => {
+    breed.style.display = breed.textContent.startsWith(selectedLetter)
+      ? 'list-item'
+      : 'none';
+  });
+});
